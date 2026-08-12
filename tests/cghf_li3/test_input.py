4b3a638d76424643671a9134285fc3b952869cb2
Nathan Gillispie <nwgllspe@memphis.edu>
2026-08-12 00:49:15 -0500

    Add psi4.energy kwarg to pass in guess orbitals; update test args
    ctest_runner does not accept binary infiles, so I just used the
    plaintext here. To avoid doing gymnastics to make input.dat work, I just
    made a way to supply guess orbitals directly from the Psithon input.

--- tests/cghf_li3/test_input.py
+++ tests/cghf_li3/test_input.py
@@ -3,5 +3,5 @@ from addons import *
 @uusing("einsums")
 @ctest_labeler("quick;cghf")
 def test_cghf_li3():
-    ctest_runner(__file__)
+    ctest_runner(__file__, extra_infiles=["li3_cocc_guess.dat"])
 
